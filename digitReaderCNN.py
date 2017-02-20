711492525212cac33299166d885bd8db6ac2e64f
Youngbin <ykim828@hotmail.com>
2017-02-19 23:57:07 -0500

    digitReaderCNN

--- digitReaderCNN.py
+++ digitReaderCNN.py
@@ -1,4 +1,29 @@
 import tensorflow as tf
+import numpy as np
+from tensorflow.examples.tutorials.mnist import input_data
+import matplotlib.pyplot as plt
+
+num_filters1 = 31
+tf.reset_default_graph()
+np.random.seed(20170218)
+tf.set_random_seed(20170218)
+
+mnist = input_data.read_data_sets("/tmp/data/", one_hot=True)
+
+x = tf.placeholder(tf.float32, [None, 784])
+x_image = tf.reshape(x, [-1,28,28,1])
+
+W_conv1 = tf.Variable(tf.truncated_normal([5,5,1,num_filters1]))
+
+
+h_conv1 = tf.nn.conv2d(x_image, W_conv1, strides=[1,1,1,1], padding='SAME')
+
+b_conv1 = tf.Variable(tf.constant(0.1, shape=[num_filters1]))
+h_conv1_cutoff = tf.nn.relu(h_conv1 + b_conv1)
+
+
+h_pool1 = tf.nn.max_pool(h_conv1_cutoff, ksize=[1,2,2,1],
+                         strides=[1,2,2,1], padding='SAME')
 
 def weight_variable(shape) :
     initial = tf.truncated_normal(shape, stddev=0.1)
@@ -7,3 +32,76 @@ def weight_variable(shape) :
 def bias_variable(shape):
     initial = tf.constant(0.1, shape=shape)
     return tf.Variable(initial)
+
+# define second layer
+num_filters2 = 64
+
+W_conv2 = tf.Variable(
+            tf.truncated_normal([5,5,num_filters1,num_filters2],
+                                stddev=0.1))
+h_conv2 = tf.nn.conv2d(h_pool1, W_conv2,
+                       strides=[1,1,1,1], padding='SAME')
+
+b_conv2 = tf.Variable(tf.constant(0.1, shape=[num_filters2]))
+h_conv2_cutoff = tf.nn.relu(h_conv2 + b_conv2)
+
+h_pool2 = tf.nn.max_pool(h_conv2_cutoff, ksize=[1,2,2,1],
+                         strides=[1,2,2,1], padding='SAME')
+
+
+h_pool2_flat = tf.reshape(h_pool2, [-1, 7 * 7 * num_filters2])
+
+num_units1 = 7 * 7 * num_filters2
+num_units2 = 1024
+
+w2 = tf.Variable(tf.truncated_normal([num_units1, num_units2]))
+b2 = tf.Variable(tf.constant(0.1, shape=[num_units2]))
+
+hidden2 = tf.nn.relu(tf.matmul(h_pool2_flat, w2) + b2)
+
+keep_prob = tf.placeholder(tf.float32)
+hidden2_drop = tf.nn.dropout(hidden2, keep_prob)
+
+w0 = tf.Variable(tf.zeros([num_units2, 10]))
+b0 = tf.Variable(tf.zeros([10]))
+k = tf.matmul(hidden2_drop, w0) + b0
+p = tf.nn.softmax(k)
+
+t = tf.placeholder(tf.float32, [None, 10])
+loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(k, t))
+train_step = tf.train.AdamOptimizer(0.0001).minimize(loss)
+
+
+correct_prediction = tf.equal(tf.argmax(p, 1), tf.argmax(t, 1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+
+sess = tf.InteractiveSession()
+sess.run(tf.global_variables_initializer())
+saver = tf.train.Saver()
+
+i = 0
+for _ in range(10000):
+    i += 1
+    batch_xs, batch_ts = mnist.train.next_batch(50)
+    sess.run(train_step, feed_dict={x:batch_xs, t:batch_ts, keep_prob:0.5})
+
+    if i % 500 == 0:
+        loss_vals, acc_vals = [], []
+        for c in range(4):
+            start = len(mnist.test.labels) / 4 * c
+            end = len(mnist.test.labels) / 4 * (c + 1)
+            loss_val, acc_val = sess.run([loss, accuracy],
+                                         feed_dict = {x:mnist.test.images[start:end],
+                                                      k:mnist.test.labels[start:end],
+                                                      keep_prob:1.0})
+            loss_vals.append(loss_val)
+            acc_vals.append(acc_val)
+        loss_val = np.sum(loss_vals)
+        acc_val = np.mean(acc_vals)
+        print ('Step: %d, Loss: %f, Accuracy: %f'
+               % (i, loss_val, acc_val))
+
+
+saver.save(sess, 'cnn_session')
+sess.close()
